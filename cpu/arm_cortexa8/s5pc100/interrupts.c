82c68e654473a2c8225c5e4b4d3e77e93eed487f
Kim, HeungJun <riverful.kim@samsung.com>
2009-05-25 20:55:03 +0900

    [S5PC100] clean code -> about timer features in interrupt.c

--- cpu/arm_cortexa8/s5pc100/interrupts.c
+++ cpu/arm_cortexa8/s5pc100/interrupts.c
@@ -43,106 +43,67 @@
 #include <s5pc1xx.h>
 #include <div64.h>
 
-static ulong timer_load_val;
+#define PRESCALER_0		(16 - 1)	/* prescaler of PWM timer 4 */
+#define MUX4_DIV_12		(2 - 1)		/* MUX 4, 1/2 period */
 
-static s5pc1xx_timers *s5pc1xx_get_base_timers(void)
-{
-	return (s5pc1xx_timers *)S5P_TIMER_BASE;
-}
+static ulong count_value;
+
+/* Internal tick units */
+static unsigned long long timestamp;	/* Monotonic incrementing timer */
+static unsigned long lastdec;	/* Last decremneter snapshot */	
 
 /* macro to read the 16 bit timer */
-static inline ulong read_timer(void)
+static inline ulong READ_TIMER(void)
 {
-	s5pc1xx_timers *const timers = s5pc1xx_get_base_timers();
+	s5pc1xx_timers *const timers = (s5pc1xx_timers *)S5P_TIMER_BASE;
 
 	return timers->TCNTO4;
 }
 
-/* Internal tick units */
-/* Last decremneter snapshot */
-static unsigned long lastdec;
-/* Monotonic incrementing timer */
-static unsigned long long timestamp;
-
 int interrupt_init(void)
 {
-	s5pc1xx_timers *const timers = s5pc1xx_get_base_timers();
+	s5pc1xx_timers *const timers = (s5pc1xx_timers *)S5P_TIMER_BASE;
 
-	/* use PWM Timer 4 because it has no ouput */
-	/* prescaler for timer 4 is 16 */
-	timers->TCFG0 = ((16-1) & 0xff) << 8;
-	if (timer_load_val == 0) {
-		/*
-		 * for 10 ms clock period @ PCLK with 4 bit divider = 1/2
-		 * (default) and prescaler = 16. Should be 20859
-		 * @66.75MHz
-		 */
-		timers->TCFG1 = ((2-1) & 0xf) << 16;
-		timer_load_val = get_PCLK() / (2 * 16 * 100);
+	/* PWM Timer 4 */
+	/* Timer Freq(HZ) = PCLK / { (prescaler_value + 1) * (divider_value) } */
+
+	/* set prescaler : 16 */
+	/* set divider : 2 */
+	timers->TCFG0 = (PRESCALER_0 & 0xff) << 8;
+	timers->TCFG1 = (MUX4_DIV_12 & 0xf) << 16;
+
+	if (count_value == 0) {
+
+		/* reset initial value */
+		/* count_value = 2085937.5(HZ) (per 1 sec)*/
+		count_value = get_PCLK() / ((PRESCALER_0 + 1) * (MUX4_DIV_12 + 1));
+
+		/* count_value / 100 = 20859.375(HZ) (per 10 msec) */
+		count_value = count_value / 100;	
 	}
 
-	/* load value for 10 ms timeout */
-	lastdec = timers->TCNTB4 = timer_load_val;
+	/* set count value */
+	timers->TCNTB4 = count_value;
+	lastdec = count_value;
 
-	/* auto load, manual update of Timer 4 */
-	timers->TCON = (timers->TCON & ~0x00700000) | TCON_4_AUTO | TCON_4_UPDATE;
+	/* auto reload & manual update */
+	timers->TCON = (timers->TCON & ~(0x03 << 20)) | S5P_TCON4_AUTO_RELOAD | S5P_TCON4_UPDATE;
 
-	/* auto load, start timer 4 */
-	timers->TCON = (timers->TCON & ~0x00700000) | TCON_4_AUTO | COUNT_4_ON;
+	/* start PWM timer 4 */
+	timers->TCON = (timers->TCON & ~(0x03 << 20)) | S5P_TCON4_AUTO_RELOAD | S5P_TCON4_ON;
+
 	timestamp = 0;
-
-	/* usb OTG */
-	S5P_VIC1INTENABLE_REG |= 1<<24;
 
 	return 0;
 }
 
+
 /*
  * timer without interrupts
  */
-
-/*
- * This function is derived from PowerPC code (read timebase as long long).
- * On ARM it just returns the timer value.
- */
-unsigned long long get_ticks(void)
-{
-	return get_timer(0);
-}
-
-ulong get_tbclk(void)
-{
-	/* We overrun in 100s */
-	return (ulong)(timer_load_val / 100);
-}
-
-void reset_timer_masked(void)
-{
-	/* reset time */
-	lastdec = read_timer();
-	timestamp = 0;
-}
-
 void reset_timer(void)
 {
 	reset_timer_masked();
-}
-
-ulong get_timer_masked(void)
-{
-	ulong now = read_timer();
-
-	if (lastdec >= now) {
-		/* normal mode */
-		timestamp += lastdec - now;
-	}
-	else {
-		/* we have an overflow ... */
-		timestamp += lastdec + timer_load_val - now;
-	}
-	lastdec = now;
-
-	return timestamp;
 }
 
 ulong get_timer(ulong base)
@@ -155,27 +116,71 @@ void set_timer(ulong t)
 	timestamp = t;
 }
 
+
+/* delay x useconds */
 void udelay(unsigned long usec)
 {
 	ulong tmo, tmp;
 
-	if (usec >= 1000) {		/* if "big" number, spread normalization to seconds */
-		tmo = usec / 1000;	/* start to normalize for usec to ticks per sec */
-		tmo *= CFG_HZ;		/* find number of "ticks" to wait to achieve target */
-		tmo /= 1000;		/* finish normalize. */
-	}
-	else {				/* else small number, don't kill it prior to HZ multiply */
-		tmo = usec * CFG_HZ;
+	if (usec >= 1000) {			/* if "big" number, spread normalization to seconds */
+		tmo = usec / 1000;		/* start to normalize for usec to ticks per sec */
+		tmo *= CONFIG_SYS_HZ;	/* find number of "ticks" to wait to achieve target */
+		tmo /= 1000;			/* finish normalize. */
+
+	} else {					/* else small number, don't kill it prior to HZ multiply */
+		tmo = usec * CONFIG_SYS_HZ;
 		tmo /= (1000 * 1000);
+
 	}
 
-	tmp = get_timer(0);		/* get current timestamp */
-	if ((tmo + tmp + 1) < tmp)	/* if setting this fordward will roll time stamp */
-		reset_timer_masked();	/* reset "advancing" timestamp to 0, set lastdec value */
-	else
+	tmp = get_timer(0);				/* get current timestamp */
+	if ((tmo + tmp + 1) < tmp) {	/* if setting this fordward will roll time stamp */
+		reset_timer_masked();		/* reset "advancing" timestamp to 0, set lastdec value */
+	} else {
 		tmo += tmp;		/* else, set advancing stamp wake up time */
+	}
 
-	while (get_timer_masked()<tmo)	/* loop till event */
-		 /*NOP*/;
+	while (get_timer_masked() < tmo);	/* loop till event */
+}
+
+void reset_timer_masked(void)
+{
+	/* reset time */
+	lastdec = READ_TIMER();
+	timestamp = 0;
+}
+
+ulong get_timer_masked(void)
+{
+	/* current tick value */
+	ulong now = READ_TIMER();
+
+	if (lastdec >= now) {
+		timestamp += lastdec - now;		/* normal mode */
+	} else {
+		timestamp += lastdec + count_value - now;	/* overflow */
+	}
+	lastdec = now;
+
+	return timestamp;
+}
+
+/*
+ * This function is derived from PowerPC code (read timebase as long long).
+ * On ARM it just returns the timer value.
+ */
+unsigned long long get_ticks(void)
+{
+	return get_timer(0);
+}
+
+/*
+ * This function is derived from PowerPC code (timebase clock frequency).  
+ * On ARM it returns the number of timer ticks per second.
+ */
+ulong get_tbclk(void)
+{
+	/* We overrun in 100s */
+	return CONFIG_SYS_HZ * 100;
 }
 
