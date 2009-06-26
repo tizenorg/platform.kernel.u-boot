e210938732a9ede84846cd7e8364fc7efce448e2
Kyungmin Park <kyungmin.park@samsung.com>
2009-06-26 10:53:32 +0900

    universal: Modify mach number to 3000
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- include/configs/s5pc100_universal.h
+++ include/configs/s5pc100_universal.h
@@ -62,7 +62,7 @@
 /*
  * Architecture magic and machine type
  */
-#define MACH_TYPE	1826
+#define MACH_TYPE	3000
 
 #define CONFIG_DISPLAY_CPUINFO
 
@@ -167,7 +167,7 @@
 	"flashboot=set bootargs root=/dev/mtdblock${bootblock}" \
 	 " rootfstype=${rootfstype}" \
 	 " ubi.mtd=${ubiblock} ${opts} " CONFIG_COMMON_BOOT "; run bootk\0" \
-	"ubifsboot=set bootargs root=ubi0!initrd.ubifs rootfstype=ubifs" \
+	"ubifsboot=set bootargs root=ubi0!rootfs rootfstype=ubifs" \
 	 " ubi.mtd=${ubiblock} ${opts} " CONFIG_COMMON_BOOT "; run bootk\0" \
 	"boottrace=setenv opts initcall_debug; run bootcmd\0" \
 	"android=set bootargs root=ubi0!ramdisk ubi.mtd=${ubiblock}" \
