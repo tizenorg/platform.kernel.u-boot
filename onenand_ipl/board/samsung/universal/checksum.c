bbd3068140c4c99211fe6f351ff807cddf5d67ab
Minkyu Kang <mk7.kang@samsung.com>
2009-07-23 17:10:02 +0900

    s5pc100: add checksum for ipl
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

---
+++ onenand_ipl/board/samsung/universal/checksum.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+
+#define IPL_8K	(0x2000 - 0x4)
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	int i, j;
+	char buf;
+	int fd;
+	unsigned int sum = 0;
+
+	fd = open(argv[1], O_RDWR);
+
+	if (fd < 0) {
+		printf("open err: %s\n", argv[1]);
+		return 1;
+	}
+
+	for (i = 0; i < IPL_8K; i++) {
+		ret = read(fd, &buf, 1);
+
+		if (ret < 0) {
+			printf("write err: %d\n", ret);
+			break;
+		}
+
+		sum += (buf & 0xFF);
+
+#if 0
+		if (i % 16 == 0)
+			printf("%04x: ", i);
+		printf("%02x ", buf & 0xFF);
+		if (i % 16 == 15)
+			printf("\n");
+#endif
+	}
+
+	ret = write(fd, &sum, 4);
+
+	if (ret < 0)
+		printf("read err: %s\n", ret);
+
+	close(fd);
+
+	printf("checksum = %x\n", sum);
+
+	return 0;
+}
