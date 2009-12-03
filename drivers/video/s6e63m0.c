55bd85311d5fb0546571f86e4ebd0b93182a34bb
daeinki <inki.dae@samsung.com>
2009-12-03 17:08:07 +0900

    s5pc110: fb: updated framebuffer and lcd panel driver.
    Signed-off-by: daeinki <inki.dae@samsung.com>

--- drivers/video/s6e63m0.c
+++ drivers/video/s6e63m0.c
@@ -42,6 +42,8 @@ struct s5pc110_gpio *gpio = (struct s5pc110_gpio *) S5PC110_GPIO_BASE;
 #define S5PCFB_C110_SDA_LOW	gpio_set_value(&gpio->gpio_mp0_4, 3, 0)
 #define S5PCFB_C110_SDA_HIGH	gpio_set_value(&gpio->gpio_mp0_4, 3, 1)
 
+#define S5PCFB_C110_SDA_READ	gpio_get_value(&gpio->gpio_mp0_4, 3)
+
 const unsigned short SEQ_PANEL_CONDITION_SET[] = {
 	0xF8, 0x01,
 	DATA_ONLY, 0x27,
@@ -108,6 +110,9 @@ const unsigned short SEQ_ETC_CONDITION_SET[] = {
 	0xf6, 0x00,
 	DATA_ONLY, 0x8c,
 	DATA_ONLY, 0x07,
+
+	/* added */
+	0xb3, 0xc,
 
 	0xb5, 0x2c,
 	DATA_ONLY, 0x12,
@@ -275,6 +280,13 @@ const unsigned short SEQ_STAND_BY_OFF[] = {
 	ENDDEF, 0x0000
 };
 
+/* added */
+const unsigned short SEQ_DISPLAY_ON[] = {
+	0x29, COMMAND_ONLY,
+
+	ENDDEF, 0x0000
+};
+
 unsigned char DELAY=1;
 
 static void s6e63m0_c110_spi_write_byte(unsigned char address, unsigned char command)
@@ -313,6 +325,72 @@ static void s6e63m0_c110_spi_write_byte(unsigned char address, unsigned char com
 }
 
 
+static unsigned char s6e63m0_c110_spi_read_byte(unsigned char select, unsigned char address)
+{
+	int     j;
+	static unsigned int first = 1;
+	unsigned char DELAY=1;
+	unsigned short data = 0;
+	char command = 0;
+
+	data = (select << 8) + address;
+
+	S5PCFB_C110_CS_HIGH;
+	S5PCFB_C110_SDA_HIGH;
+	S5PCFB_C110_CLK_HIGH;
+	udelay(DELAY);
+
+	S5PCFB_C110_CS_LOW;
+	udelay(DELAY);
+
+	for (j = PACKET_LEN + 8; j >= 0; j--)
+	{
+
+		if (j > 7) {
+			S5PCFB_C110_CLK_LOW;
+
+			/* data high or low */
+			if ((data >> (j - 8)) & 0x0001) {
+				S5PCFB_C110_SDA_HIGH;
+				printf("1");
+			} else {
+				S5PCFB_C110_SDA_LOW;
+				printf("0");
+			}
+
+			udelay(DELAY);
+			S5PCFB_C110_CLK_HIGH;
+		} else {
+			if (first) {
+				gpio_cfg_pin(&gpio->gpio_mp0_4, 3, GPIO_INPUT);
+				first = 0;
+			}
+
+			S5PCFB_C110_CLK_LOW;
+
+			if (S5PCFB_C110_SDA_READ & 0x1) {
+				command |= 1 << j;
+				printf("1");
+			} else {
+				command |= 0 << j;
+				printf("0");
+			}
+
+			udelay(DELAY);
+			S5PCFB_C110_CLK_HIGH;
+		}
+
+		udelay(DELAY);
+	}
+
+	S5PCFB_C110_CS_HIGH;
+	udelay(DELAY);
+
+	gpio_cfg_pin(&gpio->gpio_mp0_4, 3, GPIO_OUTPUT);
+
+	return command;
+}
+
 static void s6e63m0_spi_write(unsigned char address, unsigned char command)
 {
 	if (address != DATA_ONLY)
@@ -335,13 +413,17 @@ static void s6e63m0_panel_send_sequence(const unsigned short *wbuf)
 	}
 }
 
-void lcd_panel_power_on(void)
+void s6e63m0_lcd_panel_power_on(void)
 {
+	udelay(25000);
+
 	/* set gpio data for MLCD_ON to HIGH */
 	gpio_set_value(&gpio->gpio_j1, 3, 1);
 
 	/* set gpio data for MLCD_RST to HIGH */
 	gpio_set_value(&gpio->gpio_mp0_5, 5, 1);
+
+	udelay(120000);
 
 	s6e63m0_panel_send_sequence(SEQ_PANEL_CONDITION_SET);
 	s6e63m0_panel_send_sequence(SEQ_DISPLAY_CONDITION_SET);
@@ -361,16 +443,17 @@ static inline void s6e63m0_c110_panel_hw_reset(void)
 	gpio_set_value(&gpio->gpio_mp0_5, 5, 1);
 }
 
-void lcd_panel_enable(void)
+void s6e63m0_lcd_panel_enable(void)
 {
 	s6e63m0_panel_send_sequence(SEQ_STAND_BY_OFF);
+	s6e63m0_panel_send_sequence(SEQ_DISPLAY_ON);
 }
 
 static void s6e63m0_panel_disable(void)
 {
 }
 
-void lcd_panel_init(void)
+void s6e63m0_lcd_panel_init(void)
 {
 	/* set gpio pin for DISPLAY_CS to HIGH */
 	gpio_set_value(&gpio->gpio_mp0_1, 1, 1);
