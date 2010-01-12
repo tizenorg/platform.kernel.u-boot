bd60ebecbe1260d5233855164c09d83261c43aaf
Kyungmin Park <kyungmin.park@samsung.com>
2010-01-12 17:23:58 +0900

    OneNAND: Remove unused codes
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- drivers/mtd/onenand/onenand_base.c
+++ drivers/mtd/onenand/onenand_base.c
@@ -437,35 +437,6 @@ static int onenand_read_bufferram(struct mtd_info *mtd, loff_t addr, int area,
 }
 
 /**
- * onenand_sync_read_bufferram - [OneNAND Interface] Read the bufferram area with Sync. Burst mode
- * @param mtd		MTD data structure
- * @param area		BufferRAM area
- * @param buffer	the databuffer to put/get data
- * @param offset	offset to read from or write to
- * @param count		number of bytes to read/write
- *
- * Read the BufferRAM area with Sync. Burst Mode
- */
-static int onenand_sync_read_bufferram(struct mtd_info *mtd, loff_t addr, int area,
-				       unsigned char *buffer, int offset,
-				       size_t count)
-{
-	struct onenand_chip *this = mtd->priv;
-	void __iomem *bufferram;
-
-	bufferram = this->base + area;
-	bufferram += onenand_bufferram_offset(mtd, area);
-
-	this->mmcontrol(mtd, ONENAND_SYS_CFG1_SYNC_READ);
-
-	memcpy_16(buffer, bufferram + offset, count);
-
-	this->mmcontrol(mtd, 0);
-
-	return 0;
-}
-
-/**
  * onenand_write_bufferram - [OneNAND Interface] Write the bufferram area
  * @param mtd		MTD data structure
  * @param area		BufferRAM area
@@ -2171,12 +2142,6 @@ int onenand_scan(struct mtd_info *mtd, int maxchips)
 
 	if (onenand_probe(mtd))
 		return -ENXIO;
-
-	/* Set Sync. Burst Read after probing */
-	if (this->mmcontrol) {
-		printk(KERN_INFO "OneNAND Sync. Burst Read support\n");
-		this->read_bufferram = onenand_sync_read_bufferram;
-	}
 
 	/* Allocate buffers, if necessary */
 	if (!this->page_buf) {
