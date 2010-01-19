94c007e60becf50cee2f39613ee1551934291820
Joonyoung Shim <jy0922.shim@samsung.com>
2010-01-19 10:52:49 +0900

    s5p6442: smdk6442: Fix mem info
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>

--- board/samsung/smdk6442/smdk6442.c
+++ board/samsung/smdk6442/smdk6442.c
@@ -36,8 +36,10 @@ int board_init(void)
 int dram_init(void)
 {
 	gd->bd->bi_dram[0].start = PHYS_SDRAM_1;
-	gd->bd->bi_dram[0].size = get_ram_size((long *)PHYS_SDRAM_1,
-						PHYS_SDRAM_1_SIZE);
+	gd->bd->bi_dram[0].size = PHYS_SDRAM_1_SIZE;
+
+	gd->bd->bi_dram[1].start = PHYS_SDRAM_2;
+	gd->bd->bi_dram[1].size = PHYS_SDRAM_2_SIZE;
 
 	return 0;
 }
