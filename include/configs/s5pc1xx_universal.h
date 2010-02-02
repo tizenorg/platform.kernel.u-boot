ecd79ad5c25a3712a3c79573dcd7aa9ed059e8df
Minkyu Kang <mk7.kang@samsung.com>
2010-02-02 16:02:43 +0900

    s5pc110: universal: add mtd3 to bootargs
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- include/configs/s5pc1xx_universal.h
+++ include/configs/s5pc1xx_universal.h
@@ -171,13 +171,13 @@
 		" ${meminfo}" \
 		" ${mtdparts}"
 
-#define CONFIG_BOOTARGS	"root=/dev/mtdblock8 ubi.mtd=8 ubi.mtd=7" \
+#define CONFIG_BOOTARGS	"root=/dev/mtdblock8 ubi.mtd=8 ubi.mtd=7 ubi.mtd=3" \
 		" rootfstype=cramfs " CONFIG_COMMON_BOOT
 
 #define CONFIG_UPDATEB	"updateb=onenand erase 0x0 0x40000;" \
 			" onenand write 0x32008000 0x0 0x40000\0"
 
-#define CONFIG_UBI_MTD	" ubi.mtd=${ubiblock} ubi.mtd=7"
+#define CONFIG_UBI_MTD	" ubi.mtd=${ubiblock} ubi.mtd=7 ubi.mtd=3"
 
 #define CONFIG_UBIFS_OPTION	"rootflags=bulk_read,no_chk_data_crc"
 
