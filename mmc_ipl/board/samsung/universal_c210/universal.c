d0b34ce40680f91078b022a9af04cd7875d3b307
Jaehoon Chung <jh80.chung@samsung.com>
2010-08-22 19:24:52 +0900

    s5pc210: mmc_ipl: code clean up (not yet work) Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>

--- mmc_ipl/board/samsung/universal_c210/universal.c
+++ mmc_ipl/board/samsung/universal_c210/universal.c
@@ -26,42 +26,11 @@
 
 #include "mmc_ipl.h"
 
-#define EMMC_COPY_TO_MEM_ADDR	0xD0037F9C
-
-#define BLK_COUNT		512
-#define BUS_WIDTH_8BIT		8
-
-typedef u32(*copy_emmc_to_mem)
-	(u32 ack, u32 number_of_block, u32 *buf, int buswidth);
-
 int board_mmc_init(void)
 {
-	unsigned int value;
-
-	/* MASSMEMORY_EN: XMSMDATA7: GPJ2[7] output high */
-	value = readl(0xe0200280);
-	value &= ~(0xf << (7 << 2));
-	value |= (1 << (7 << 2));
-	writel(value, 0xe0200280);
-
-	value = readl(0xe0200284);
-	value |= (1 << 7);
-	writel(value, 0xe0200284);
-
-	/* GPG0[0:6] special function 2 */
-	writel(0x2222022, 0xe02001a0);
-	/* GPG0[0:6] pull disable */
-	writel(0x10, 0xe02001a8);
-	/* GPG0[0:6] drv 4x */
-	writel(0x3fef, 0xe02001ac);
-
 	return 0;
 }
 
 void board_mmc_read_block(unsigned char *buf)
 {
-	copy_emmc_to_mem copy_bl2 =
-		(copy_emmc_to_mem) (*(u32 *) EMMC_COPY_TO_MEM_ADDR);
-
-	copy_bl2(0, BLK_COUNT, (u32 *) buf, BUS_WIDTH_8BIT);
 }
