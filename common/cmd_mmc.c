771e107cd478c637e21cd86d83e987b7cef5fc1d
Minkyu Kang <mk7.kang@samsung.com>
2010-10-20 21:04:33 +0900

    mmc: fix the showing capacity of mmc
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- common/cmd_mmc.c
+++ common/cmd_mmc.c
@@ -109,7 +109,7 @@ static void print_mmcinfo(struct mmc *mmc)
 	printf("High Capacity: %s\n", mmc->high_capacity ? "Yes" : "No");
 
 	if (mmc->high_capacity)
-		printf("Capacity: %lld MByte\n", mmc->capacity >> 11);
+		printf("Capacity: %lld MByte\n", mmc->capacity >> 20);
 	else
 		printf("Capacity: %lld\n", mmc->capacity);
 
