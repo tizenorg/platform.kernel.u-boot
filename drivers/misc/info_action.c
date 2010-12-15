5ce3662c2b365efa7bc14a6294eb67d60c76e1d3
Jaehoon Chung <jh80.chung@samsung.com>
2010-12-15 13:07:21 +0900

    s5p: add init mmc device for sdcard boot Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>

--- drivers/misc/info_action.c
+++ drivers/misc/info_action.c
@@ -4,12 +4,14 @@
  */
 #include <common.h>
 #include <info_action.h>
+#include <mmc.h>
 
 void info_action_check(void)
 {
 	struct info_action *ia = (struct info_action *) CONFIG_INFO_ADDRESS;
 	char buf[64];
 	unsigned int count = 0;
+	struct mmc *mmc;
 
 	if (ia->magic != INFO_ACTION_MAGIC)
 		return;
@@ -17,10 +19,22 @@ void info_action_check(void)
 	switch (ia->action) {
 	case INFO_ACTION_SDCARD_BOOT:
 		puts("sdcard boot\n");
+
+		/*
+		 * We need to initialize SD card for SD-boot.
+		 * SD card device index : 2
+		 */
+		mmc = find_mmc_device(2);
+		if (!mmc) {
+			printf("Not found SD-card..insert SD-card!!\n");
+			return;
+		}
+		mmc_init(mmc);
+
 		/* Please add the parameters and just 'run mmcboot' */
 		count += sprintf(buf + count, "run mmcboot; ");
 		setenv("bootcmd", buf);
-		setenv("mmcdev", "1");
+		setenv("mmcdev", "2");
 		setenv("mmcrootpart", "0");
 		break;
 	case INFO_ACTION_LCD_CONSOLE:
