520665411e9828e5c5cc2aa235969c93ad8420c6
Lukasz Majewski <l.majewski@samsung.com>
2012-04-18 16:56:32 +0200

    usb:gadget:composite USB composite gadget support
    USB Composite gadget implementation for u-boot. It builds on top
    of USB UDC drivers.
    
    This commit is based on following files from Linux Kernel v2.6.36:
    
    ./include/linux/usb/composite.h
    ./drivers/usb/gadget/composite.c
    
    SHA1: d187abb9a83e6c6b6e9f2ca17962bdeafb4bc903
    
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Marek Vasut <marex@denx.de>
    
    ---
    Changes for v2:
    - Squash the strict kernel files with u-boot compatibility layer.
    - Removal of dead/kernel specific code
    - Comments corrected according to u-boot coding style

--- include/usb/lin_gadget_compat.h
+++ include/usb/lin_gadget_compat.h
@@ -36,8 +36,8 @@
 #define mutex_lock(...)
 #define mutex_unlock(...)
 
-#define WARN_ON(x) if (x) {printf("WARNING in %s line %d\n" \
-				  , __FILE__, __LINE__); }
+#define WARN_ON(x) do { if (x) printf("WARNING in %s line %d\n"	  \
+				      , __FILE__, __LINE__); } while (0)
 
 #define KERN_WARNING
 #define KERN_ERR
@@ -56,6 +56,26 @@
 
 #define __iomem
 #define min_t min
+
+#define BITS_PER_BYTE				8
+#define BITS_TO_LONGS(nr) \
+	DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(long))
+#define DECLARE_BITMAP(name, bits) \
+	unsigned long name[BITS_TO_LONGS(bits)]
+
+#define small_const_nbits(nbits) \
+	(__builtin_constant_p(nbits) && (nbits) <= BITS_PER_LONG)
+
+static inline void bitmap_zero(unsigned long *dst, int nbits)
+{
+	if (small_const_nbits(nbits))
+		*dst = 0UL;
+	else {
+		int len = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
+		memset(dst, 0, len);
+	}
+}
+
 #define dma_cache_maint(addr, size, mode) cache_flush()
 void cache_flush(void);
 
