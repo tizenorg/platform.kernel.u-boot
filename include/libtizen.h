eca387ee164ddafd5f11cfc0349adecdef3c6c38
Przemyslaw Marczak <p.marczak@samsung.com>
2014-02-07 15:03:41 +0100

    libtizen: add download logo and download progress bar
    Added features:
    - add screen with exit info if no cable is connected
    - download logo with progress bar after after THOR init
    - downlaod failure screen on error
    - PIT version, U-boot version on download screen
    - function for update progess bar when downloading is going on
    
    Signed-off-by: Hyungwon Hwang <human.hwang@samsung.com>
    Signed-off-by: Inha Song <ideal.song@samsung.com>
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>

--- include/libtizen.h
+++ include/libtizen.h
@@ -8,8 +8,15 @@
 #ifndef _LIBTIZEN_H_
 #define _LIBTIZEN_H_
 
+#include <lcd.h>
+
 #define HD_RESOLUTION	0
 
 void get_tizen_logo_info(vidinfo_t *vid);
+void draw_thor_progress(unsigned long long int total_file_size, unsigned long long int downloaded_file_size);
+void draw_thor_init_screen(void);
+void draw_thor_screen(void);
+void draw_thor_fail_screen(void);
+void draw_thor_cable_info(void);
 
 #endif	/* _LIBTIZEN_H_ */
