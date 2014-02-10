651de3ee577d90ad888200cda277f4ef4ce935dc
Przemyslaw Marczak <p.marczak@samsung.com>
2014-02-10 16:28:30 +0100

    samsung: misc: check_keys(), key_pressed() - remove type static.
    This patch removes type "static" from those functions declaration.
    
    Change-Id: I2244ca3568b73251401e5102f26cc113ea69c1a4
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>

--- board/samsung/common/misc.c
+++ board/samsung/common/misc.c
@@ -15,7 +15,6 @@
 #include <linux/sizes.h>
 #include <asm/arch/cpu.h>
 #include <asm/gpio.h>
-#include <linux/input.h>
 #include <power/pmic.h>
 #include <mmc.h>
 
@@ -120,7 +119,7 @@ static int power_key_pressed(u32 reg)
 	return !!(status & mask);
 }
 
-static int key_pressed(int key)
+int key_pressed(int key)
 {
 	int value;
 
@@ -142,7 +141,7 @@ static int key_pressed(int key)
 	return value;
 }
 
-static int check_keys(void)
+int check_keys(void)
 {
 	int keys = 0;
 
