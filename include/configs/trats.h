33e129d5e4770ea90697f065b51967783144ead5
Przemyslaw Marczak <p.marczak@samsung.com>
2014-02-17 11:28:03 +0100

    MAINLINE trats/trats2: enable support of exynos security subsystem and function hw_rand()
    This allows to use exynos random number generator.
    
    Change-Id: Ic179ddb70fd4a132a1780a558e1873e27c8e64eb
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>

--- include/configs/trats.h
+++ include/configs/trats.h
@@ -321,6 +321,10 @@
 #define CONFIG_USB_GADGET_VBUS_DRAW	2
 #define CONFIG_USB_CABLE_CHECK
 
+/* Security subsystem - enable hw_rand() */
+#define CONFIG_EXYNOS_ACE_SHA
+#define CONFIG_RAND_HW_ACCEL
+
 /* Common misc for Samsung */
 #define CONFIG_MISC_COMMON
 
