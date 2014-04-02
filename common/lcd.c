ac4475fb8cd01ad7c8b2dc9a4037b7cd31329724
Przemyslaw Marczak <p.marczak@samsung.com>
2014-04-02 15:21:06 +0200

    common: lcd: extend lcd api by function lcd_get_position_cursor()
    This commit change the name of function lcd_position_cursor()
    to lcd_set_position_cursor() and adds its complementary function
    lcd_get_position_cursor().
    This change is useful e.g. when user wants overwrite some lines,
    words or even just a letter on lcd console screen.
    
    Other changes:
    - update previous lcd_position_cursor() calls
    
    Change-Id: I4a102c236f2d2d5c238e9790382e4b9454e3c361
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>
    Cc: Vadim Bendebury <vbendeb@chromium.org>
    Cc: Simon Glass <sjg@chromium.org>

--- common/lcd.c
+++ common/lcd.c
@@ -1166,10 +1166,16 @@ static int on_splashimage(const char *name, const char *value, enum env_op op,
 U_BOOT_ENV_CALLBACK(splashimage, on_splashimage);
 #endif
 
-void lcd_position_cursor(unsigned col, unsigned row)
+void lcd_set_position_cursor(unsigned col, unsigned row)
 {
 	console_col = min(col, CONSOLE_COLS - 1);
 	console_row = min(row, CONSOLE_ROWS - 1);
+}
+
+void lcd_get_position_cursor(unsigned *col, unsigned *row)
+{
+	*col = console_col;
+	*row = console_row;
 }
 
 int lcd_get_pixel_width(void)
