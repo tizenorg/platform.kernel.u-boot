44a9d8f790d0547f3756abbe80a496b98a26359a
Przemyslaw Marczak <p.marczak@samsung.com>
2014-04-02 16:13:59 +0200

    lib: tizen: thor screen update
    Now cable connection message is displayed on thor screen.
    When cable is connected then only unneeded lcd console lines are overwritten.
    
    Main changes:
    - add new function: draw_thor_connected()
    - update function draw_thor_screen()
    - usb: gadget: thor:
      - move call to draw_thor_screen(),
      - add call to draw_thor_connected()
    
    other change:
    - remove draw_thor_init_screen() function
    
    Change-Id: Ifc33f63eeb4822551fc78af1c6dd2a03044cab7e
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>

--- drivers/usb/gadget/f_thor.c
+++ drivers/usb/gadget/f_thor.c
@@ -712,7 +712,7 @@ int thor_init(void)
 	int power_key_cnt = 0;
 
 #ifdef CONFIG_TIZEN
-	draw_thor_init_screen();
+	draw_thor_screen();
 #endif
 	/* Wait for a device enumeration and configuration settings */
 	debug("THOR enumeration/configuration setting....\n");
@@ -729,7 +729,7 @@ int thor_init(void)
 		}
 	}
 #ifdef CONFIG_TIZEN
-	draw_thor_screen();
+	draw_thor_connected();
 #endif
 	thor_set_dma(thor_rx_data_buf, strlen("THOR"));
 	/* detect the download request from Host PC */
