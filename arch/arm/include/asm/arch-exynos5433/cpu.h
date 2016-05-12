2fde764171f8bdad36630d1117ba842600f75903
Lukasz Majewski <l.majewski@samsung.com>
2016-05-12 13:17:35 +0200

    TPL: TM2: Extend cpu.h header file to support DWC3 USB controller
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>

--- arch/arm/include/asm/arch-exynos5433/cpu.h
+++ arch/arm/include/asm/arch-exynos5433/cpu.h
@@ -15,7 +15,8 @@
 /* EXYNOS5433 */
 #define EXYNOS5433_CLOCK_BASE		0x10040000
 #define EXYNOS5433_PWMTIMER_BASE	0x14DD0000
-
+#define EXYNOS5433_USB3PHY_BASE         0x15500000
+#define EXYNOS5433_POWER_BASE		0x105C0000
 
 #ifndef __ASSEMBLY__
 #include <asm/io.h>
@@ -47,6 +48,8 @@ static inline unsigned long __attribute__((no_instrument_function)) \
 
 SAMSUNG_BASE(clock, CLOCK_BASE)
 SAMSUNG_BASE(timer, PWMTIMER_BASE)
+SAMSUNG_BASE(usb3_phy, USB3PHY_BASE)
+SAMSUNG_BASE(power, POWER_BASE)
 #endif
 
 #endif	/* _EXYNOS64_CPU_H */
